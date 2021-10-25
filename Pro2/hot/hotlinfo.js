80c03fed82aa0ed84a1d1b50bbc682882c309a8a
ghada1999 <Ghada.Alshamrani.eam.js@tuwaiq.ed>
2021-10-25 16:58:14 +0300

    new chang

---
+++ Pro2/hot/hotlinfo.js
@@ -0,0 +1,32 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Abha Palace ",
+    desc : "5 stars Hotel ",
+    img : "f11.jpg",
+    price : 600
+  },
+  
+  
+  124: {
+    name : "Shafa Abha",
+    desc : " 4 stars Hotel.",
+    img : "f2.jpg",
+    price :30
+  },
+  
+  
+  125: {
+    name : " Azd Hotel",
+    desc : "4 stars Hotel.",
+    img : "f4.jpg",
+    price : 40
+  },
+  126: {
+    name : "Sama Abha ",
+    desc : "5 Sars Hotel  ",
+    img : "f3.jpg",
+    price : 50
+  }
+//  
+};
